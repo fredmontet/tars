6b1fb859fbd2c0d3963556d8e84d1d57ff3c5e3b
Frédéric Montet <frederic.montet@vast.ch>
2021-07-31 09:40:22 +0200

    Add comments and typings

--- src/tars/utils/metrics.py
+++ src/tars/utils/metrics.py
@@ -1,39 +1,72 @@
+"""
+Some metrics taken from :
+- https://www.investopedia.com/articles/fundamental-analysis/10/strategy-performance-reports.asp
+"""
 
 
-def total_net_profit(gross_profit, gross_loss):
-    """
-    Gross Profit − Gross Loss = Total Net Profit
-    
-    https://www.investopedia.com/articles/fundamental-analysis/10/strategy-performance-reports.asp
+def total_net_profit(gross_profit: float, gross_loss: float) -> float:
+    """ Total net profit
+
+    .. math::
+        Gross Profit − Gross Loss = Total Net Profit
+
+    :param gross_profit: float of the gross profit
+    :param gross_loss: float of the gross loss
+    :return: float of the total net profit
     """
     return gross_profit - gross_loss
 
 
-def profit_factor(gross_profit, gross_loss):
-    """
-    Gross Profit / Gross Loss = Profit Factor
+def profit_factor(gross_profit: float, gross_loss: float) -> float:
+    """ Profit factor
+
+    .. math::
+        Gross Profit / Gross Loss = Profit Factor
+
+    :param gross_profit: float of the gross profit
+    :param gross_loss: float of the gross loss
+    :return: float of the profit factor
     """
     return gross_profit / gross_loss
 
 
-def percent_profitable(winning_trades, total_trades):
-    """
-    Winning Trades / Total Trades = Percent Profitable
+def percent_profitable(winning_trades: int, total_trades: int) -> float:
+    """ Percent profitable
+
+    .. math::
+        Winning Trades / Total Trades = Percent Profitable
+
+    :param winning_trades: int of the number of winning trades
+    :param total_trades: int of the total number of trades executed
+    :return: float of the profitability in percent (1.0 = 100%)
     """
     return winning_trades / total_trades
 
 
-def average_trade_net_profit(total_net_profit, total_trades):
-    """
-    Total Net Profit / Total Trades = Trade Net Profit
+def average_trade_net_profit(total_net_profit: float, total_trades: int) \
+        -> float:
+    """ Average trade net profit
+
+    .. math::
+        Total Net Profit / Total Trades = Trade Net Profit
+
+    :param total_net_profit: float of the total net profit
+    :param total_trades: int of the total number of trades executed
+    :return float of the average trade net profit
     """
     return total_net_profit / total_trades
 
 
-def win_loss_ratio(winning_trades, losing_trades):
-    """
-    Winning Trades / Losing Trades
+def win_loss_ratio(winning_trades: float, losing_trades: float) -> float:
+    """ Win/loss ratio
 
-    https://www.investopedia.com/terms/w/win-loss-ratio.asp
+    Taken from https://www.investopedia.com/terms/w/win-loss-ratio.asp
+
+    .. math::
+        Winning Trades / Losing Trades = Win Loss Ratio
+
+    :param winning_trades: int of the number of winning trades
+    :param losing_trades: int of the number of losing trades
+    :return float of the win/loss ratio
     """
     return winning_trades / losing_trades
--- src/tars/utils/runner.py
+++ src/tars/utils/runner.py
@@ -1,16 +1,30 @@
+import logging
+from typing import Callable, NoReturn
 from pandas import Timestamp, Timedelta
 from time import sleep
 
 
 class Runner:
+    """
+    A Runner represent an object able to execute a function through time.
+
+    The function can be executed with a chosen frequency e.g. every 10 seconds
+    and for a optional duration e.g. 2 hours.
+
+    :ivar is_running : Boolean describing if the Runner is running or not.
+    """
       
     def __init__(self):
         self.is_running = False
-    
-    def stop(self):
-        self.is_running = False
 
-    def start(self, func, frequency, duration=None):
+    def start(self, func: Callable, frequency: str, duration: str = None) \
+            -> NoReturn:
+        """ Start the Runner
+
+        :param func: The function to be executed
+        :param frequency: String representing a frequency in the same form than a Pandas' Timedelta (https://pandas.pydata.org/docs/reference/api/pandas.Timedelta.html)
+        :param duration: String representing a frequency in the same form than a Pandas' Timedelta (https://pandas.pydata.org/docs/reference/api/pandas.Timedelta.html)
+        """
         self.is_running = True
 
         if duration is not None:
@@ -22,3 +36,11 @@ class Runner:
                     break
             func()
             sleep(Timedelta(frequency).total_seconds())
+
+        logging.debug(f'Runner started with frequency of {frequency} and '
+                      f'duration of {duration}')
+
+    def stop(self) -> NoReturn:
+        """ Stop the Runner """
+        self.is_running = False
+        logging.debug(f'Runner stopped')
