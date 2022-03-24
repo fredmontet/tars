a7d3a54de824551e88ac828e986e14e4e4988566
Frédéric Montet <frederic.montet@vast.ch>
2022-03-24 10:03:58 +0100

    Add koker strategy in notebook, soon ready for prod

--- src/tars/strategies/abstract_strategy.py
+++ src/tars/strategies/abstract_strategy.py
@@ -8,7 +8,7 @@ class AbstractStrategy(ABC):
     The AbstractStrategy represents the method contract of a trading
     strategy of taken by a trader to manage a portfolio.
     """
-        
+
     @abstractmethod
     def run(self):
         pass
